166c18729998cab263f7bf58ab31435351a4796f
Vashu <vashuantaldhaliyan@gmail.com>
2026-06-25 15:43:54 +0530

    Added Program to Check Alphabet, Digit, or Symbol

---
+++ Conditionals/Conditional_11.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+    {
+        printf("It is an alphabet");
+    }
+    else if (ch >= '0' && ch <= '9')
+    {
+        printf("It is a digit");
+    }
+    else
+    {
+        printf("It is a symbol");
+    }
+
+    return 0;
+}
