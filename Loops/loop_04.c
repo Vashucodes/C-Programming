be438c2bad02328b602b30e18846555cd4ff0e99
Vashu <vashuantaldhaliyan@gmail.com>
2026-07-02 20:36:54 +0530

    Added first and last digit program

---
+++ Loops/loop_04.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n, b, c;
+
+    printf("Enter any number: ");
+    scanf("%d", &n);
+
+    if (n < 0)
+        n = -n;
+
+    c = n % 10;
+
+    while (n != 0) {
+        b = n % 10;
+        n = n / 10;
+    }
+
+    printf("First digit = %d and Last digit = %d", b, c);
+
+    return 0;
+}
