0e511e8301da54743d94f6df32dfbac0e43103a8
Vashu <vashuantaldhaliyan@gmail.com>
2026-07-12 21:02:59 +0530

    Added program to print even number from 1 to 20 using for loop

---
+++ Loops/loop_21.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    printf("Even number b/ 1 and 20 is iven below\n");
+
+    for (int i = 0; i <= 20; i+=2)
+    {
+        printf("%d\n",i);
+    }
+    
+    return 0;
+}
